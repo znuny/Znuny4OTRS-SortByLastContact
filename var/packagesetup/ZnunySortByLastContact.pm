097801e7cc3cf3de31fb34ecc6350938bf7114ae
Denny Korsukéwitz <dennykorsukewitz@gmail.com>
2025-02-27 21:30:18 +0000

    InternalField flag has been added for dynamic fields as well as for upgrades of existing fields.

--- var/packagesetup/ZnunySortByLastContact.pm
+++ var/packagesetup/ZnunySortByLastContact.pm
@@ -14,8 +14,9 @@ use warnings;
 use utf8;
 
 our @ObjectDependencies = (
-    'Kernel::System::ZnunyHelper',
+    'Kernel::System::DB',
     'Kernel::System::DynamicField',
+    'Kernel::System::ZnunyHelper',
 );
 
 use Kernel::System::VariableCheck qw(:all);
@@ -62,11 +63,13 @@ sub CodeInstall {
 
     my @DynamicFields = (
         {
-            Name       => 'TicketLastCustomerContactTime',
-            Label      => "TicketLastCustomerContactTime",
-            ObjectType => 'Ticket',
-            FieldType  => 'DateTime',
-            Config     => {
+            Name  => 'TicketLastCustomerContactTime',
+            Label => "TicketLastCustomerContactTime",
+
+            InternalField => 1,
+            ObjectType    => 'Ticket',
+            FieldType     => 'DateTime',
+            Config        => {
                 'DefaultValue'  => 0,
                 'Link'          => '',
                 'YearsInFuture' => 5,
@@ -75,11 +78,13 @@ sub CodeInstall {
             },
         },
         {
-            Name       => 'TicketLastCustomerContactDirection',
-            Label      => "TicketLastCustomerContactDirection",
-            ObjectType => 'Ticket',
-            FieldType  => 'Dropdown',
-            Config     => {
+            Name  => 'TicketLastCustomerContactDirection',
+            Label => "TicketLastCustomerContactDirection",
+
+            InternalField => 1,
+            ObjectType    => 'Ticket',
+            FieldType     => 'Dropdown',
+            Config        => {
                 PossibleValues => {
                     'agent'    => 'agent',
                     'customer' => 'customer'
@@ -128,6 +133,30 @@ sub CodeUpgrade {
     my ( $Self, %Param ) = @_;
 
     my $DynamicFieldObject = $Kernel::OM->Get('Kernel::System::DynamicField');
+    my $DBObject           = $Kernel::OM->Get('Kernel::System::DB');
+
+    my @DynamicFieldList = ( 'TicketLastCustomerContactTime', 'TicketLastCustomerContactDirection' );
+
+    # Upgrade dynamic fields to internal fields.
+    # At the moment you can not update 'InternalField' via the DynamicField API.
+    # So we have to do it manually via SQL.
+    DYNAMICFIELD:
+    for my $DynamicField (@DynamicFieldList) {
+        my $DynamicFieldConfig = $DynamicFieldObject->DynamicFieldGet(
+            Name => $DynamicField,
+        );
+        next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
+
+        # Set internal field.
+        $DynamicFieldConfig->{InternalField} = 1;
+
+        next DYNAMICFIELD if !$DBObject->Do(
+            SQL  => 'UPDATE dynamic_field SET internal_field = ? WHERE id = ?',
+            Bind => [
+                \$DynamicFieldConfig->{InternalField}, \$DynamicFieldConfig->{ID},
+            ],
+        );
+    }
 
     # Upgrade dynamic field type.
     my $DynamicFieldConfig = $DynamicFieldObject->DynamicFieldGet(
